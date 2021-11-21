77b9da3fc9256c8c9de919775d8a6b07c39754ec
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2021-11-21 15:08:57 +0100

    name changes

---
+++ 1.1.Calibration.R
@@ -0,0 +1,13 @@
+## FlexCAT project
+## (1) LCA: Calibration
+## Tasos Psychogyiopoulos
+
+## libraries
+library(tidyverse)
+library(poLCA)
+library(patchwork)
+## Data + Functions
+setwd('/Users/tasospsy/Google Drive/_UvA/Master Thesis/FlexCAT/')
+source("0.Cleaning-Subsetting.R")
+source("0.Functions.R")
+source("0.FunctionsViz.R")
