8a6b2512de1a96cfbc9b1bc912597834113cd063
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2022-04-30 17:54:28 +0200

    new plot KL with ICs

--- NEW_xANALYSIS.R
+++ NEW_xANALYSIS.R
@@ -2,7 +2,11 @@
 ## 1st STEP Khat - K
 ## ------------------
 
+## from R -server
 setwd("/home/rstudio")
+## from mac
+setwd("/Users/tasospsy/Google Drive/_UvA/Master Thesis/")
+
 source("FlexCAT/0.Functions.R")
 ## Load files output from EFS
 setwd("/home/rstudio/efs")
@@ -91,7 +95,7 @@ theme1 <- theme(plot.background = element_rect(fill = "white", color = NA), #bac
 
 
 ## ------------------
-## 2nd STEP p Vs phat
+## 2nd STEP p Vs pha
 ## ------------------
 library(philentropy)
 ## Add density of sum scores (dens.ss) in TRUE MODELS
@@ -120,8 +124,81 @@ testKL <- out2.td %>%
                                           epsilon = 0.000000001)) %>% 
   dplyr::select(-R, -true.dens, -est.dens)
 
-setwd("/home/rstudio/efs")
-save(testKL,file ='testKL.Rdata')
+#setwd("/home/rstudio/efs")
+#save(testKL,file ='testKL.Rdata')
+
+## which is the maximum percentage of etsimeted K that is 
+## picked by each IC, within each condition of the 24?
+perc.ICs.K <- out.td %>% 
+  rowwise() %>% mutate_at(vars(AIC,AIC3,BIC,aBIC),  ~.x + K)  %>% 
+  group_by(TrueMod, N, K, J) %>% 
+  pivot_longer(cols = c(AIC,AIC3,BIC,aBIC), 
+               names_to = 'IC', 
+               values_to = 'est.K') %>% 
+  group_by(TrueMod, N, K, J,IC) %>% 
+  count(est.K) %>% 
+  filter(n == max(n)) %>% 
+  mutate(n = unique(n)) %>% 
+  pivot_wider(names_from = IC, 
+              values_from = n)
+
+## The same in long format for plots
+perc.ICs.K.LONG <- perc.ICs.K %>% 
+  pivot_longer(cols = c(AIC,AIC3,BIC,aBIC), 
+                                names_to = 'IC', 
+                                values_to = 'percentage') %>% 
+  filter(!is.na(percentage)) %>% 
+  mutate(J = case_when(J== 7 ~ 'J = 7',
+                       J== 15 ~ 'J = 15'),
+         K = case_when(K == 4 ~  'K = 4',
+                       K == 8 ~  'K = 8',
+                       K == 12 ~ 'K = 12')) %>% 
+  mutate(IC.y = case_when(IC == 'AIC' ~ .6,
+                          IC == 'AIC3' ~ .5,
+                          IC == 'BIC' ~ .4,
+                          IC == 'aBIC' ~ .3))
+## PLOT KL smooth spline per condition 
+## ?? (WITH percentage of ICs)
+testKL %>% 
+  mutate(J = case_when(J== 7 ~ 'J = 7',
+                       J== 15 ~ 'J = 15'),
+         K = case_when(K == 4 ~  'K = 4',
+                       K == 8 ~  'K = 8',
+                       K == 12 ~ 'K = 12')) %>%
+  mutate(t.K = as.integer(stringr::str_extract(K, "\\d+"))) %>% 
+  ggplot() +
+  geom_point(aes(x = est.K, y = KL.p), size = .8, color = 'lightblue') +
+  geom_smooth(aes(x = est.K, y = KL.p), method = 'loess', 
+              color = 'grey5', size = 1) +
+  geom_vline(aes(xintercept= t.K), linetype="dotted", 
+                        color = "black", size=.4)+
+  
+  ## ICs part
+  #geom_label(data = perc.ICs.K.LONG,
+  #  aes(x = est.K, y = IC.y,
+  #      label = paste0(IC, ': K=',est.K,' (',percentage,'%)'), color = IC),
+  #  label.size = 0,
+  #  size = 3,
+  #  #color = "black",
+  #  #fill="lightgreen", 
+  #  alpha = .9, 
+  #  hjust = 0) +
+  geom_vline(
+    data = perc.ICs.K.LONG, 
+      aes(xintercept= est.K, color = IC), size=1, alpha = .5, linetype = 'solid') +
+  
+  ## 
+  facet_grid(fct_relevel(N,'500','1000','2000', '5000') ~ 
+               fct_relevel(J, 'J = 7', 'J = 15') +
+               fct_relevel(K, 'K = 4', 'K = 8', 'K = 12'),
+             scales = 'free_x') + 
+  labs(title = "",
+       x = "Number of estimated classes",
+       y = "Kullbach - Leibler distance (π - est. π)") +
+  theme_bw() + theme1 +
+  theme(axis.text.x = element_text(size=11, color = "black"), 
+        #legend.position = 'none'
+        )
 
 ## --- UNDER CON
 
